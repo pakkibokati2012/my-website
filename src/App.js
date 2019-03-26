70f6085f69264dd18235f810d9670029298562bf
pakkibokati2012@gmail.com <pakkibokati2012@gmail.com>
2019-03-26 13:56:23 +0545

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import { Link } from 'react-router-dom';
 import './App.css';
+import Main from './components/main';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='demo-big-content'>
+        <Layout>
+          <Header title='Title' scroll>
+            <Navigation>
+              <Link to='/resume'>Resume</Link>
+              <Link to='/projects'>Projects</Link>
+              <Link to='/contact'>Contact</Link>
+              <Link to='/aboutme'>About Me</Link>
+            </Navigation>
+          </Header>
+          <Drawer title='Title'>
+            <Navigation>
+              <Link to='/resume'>Resume</Link>
+              <Link to='/projects'>Projects</Link>
+              <Link to='/contact'>Contact</Link>
+              <Link to='/aboutme'>About Me</Link>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className='page-content' />
+            <Main />
+          </Content>
+        </Layout>
       </div>
     );
   }
